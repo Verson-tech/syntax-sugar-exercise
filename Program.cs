319b8ac7a48e1f3a55318d3508aad5c56aa38d52
Vira Murray <vira.avramenko@gmail.com>
2022-09-15 12:54:35 -0400

    exercise complete

--- Program.cs
+++ Program.cs
@@ -1,16 +1,8 @@
-﻿var answer = 4;
-//string response;
+﻿
+//inferred typing is where the compiler defines the variable's type
+var answer = 4;
 
-//if (answer >  9)
-//{
-//    response = answer + " is less than nine";
-//}
-//else
-//{
-//    response = answer + "greater than or equal to nine";
-//}
-//Console.WriteLine(response);
-
-var response = (answer <= 9) ? $"{answer} is less than nine" : $"{answer} greater or equal to nine";
+//ternary operator and string interpolation
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater or equal to nine";
 
 Console.WriteLine(response);
